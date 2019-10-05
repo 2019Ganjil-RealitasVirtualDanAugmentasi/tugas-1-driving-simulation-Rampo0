2018df43c05c2fad20379674259103fa0fc9701d
anwar <weissman1505199@gmail.com>
2019-10-05 14:03:10 +0700

    fdgfg

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -2,59 +2,89 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarMovement : MonoBehaviour
+public class CarMovement : Controller
 {
-	// Start is called before the first frame update
-
 	private Rigidbody rb;
 	[SerializeField] private float speed = 5f;
 	[SerializeField] private float sensivity = 2f;
-	//private Vector3 direction;
+	private float frontAxis;
+	private float horiAxis;
+	private float steerAxis;
+	private float brakeAxis;
+	private float gasAxis;
+	[SerializeField] private Transform steer;
+	float maxAngle = 45f;
+	bool steerType = false;
 
-    void Start()
+    void Awake()
     {
 		rb = GetComponent<Rigidbody>();    
     }
 
-    // Update is called once per frame
     void Update()
     {
-		//Debug.Log(Input.GetAxis("xbox"));
-		//Debug.Log(Input.GetAxis("gas"));
-		float frontAxis = Input.GetAxis("gas");
-		float horiAxis = Input.GetAxis("xbox");
+		if (controller == InputType.Xbox)
+		{
+			frontAxis = Input.GetAxis("gas");
+			horiAxis = Input.GetAxis("xbox");
+		}
+		else if (controller == InputType.Keyboard)
+		{
+			frontAxis = Input.GetAxis("Vertical");
+			horiAxis = Input.GetAxis("Horizontal");
+		}
+		else if (controller == InputType.Steer) {
+			steerAxis = Input.GetAxis("Steer");
+			brakeAxis = Input.GetAxis("Brake");
+			gasAxis = Input.GetAxis("GasSteer");
+			steerType = true;
+		}
 
 		//transform.Translate(new Vector3(0, 0, frontAxis * speed * Time.deltaTime), Space.Self);
-		rb.velocity = transform.forward * frontAxis * speed;
-		
-		
-		//rb.AddForce(transform.forward * frontAxis * speed * speed, ForceMode.Acceleration);
 
-		/*if (frontAxis > 0)
-		{
-			rb.velocity = transform.forward * frontAxis * speed;
-		//	direction = new Vector3
-			
-		}
-		else {
-			rb.velocity = transform.forward * frontAxis * speed;
-		}*/
-
-
-		float direction = Vector3.Dot(transform.forward, rb.velocity.normalized);
-		Debug.Log(direction);
+		/*float direction = Vector3.Dot(transform.forward, rb.velocity.normalized);
 
 		if (direction < 0)
 		{
 			transform.Rotate(new Vector3(0, -1 * horiAxis * sensivity, 0), Space.Self);
 		}
-		else if(direction > 0){
+		else if (direction > 0)
+		{
 			transform.Rotate(new Vector3(0, horiAxis * sensivity, 0), Space.Self);
+		}*/
+
+		//steer.Rotate(new Vector3(0, -(Mathf.Lerp(horiAxis,  horiAxis * 1.5f, 0.1f)), 0), Space.Self);
+		//Debug.Log(steer.transform.eulerAngles);
+
+		if (!steerType)
+		{
+			float angleRotation = horiAxis * maxAngle;
+			steer.localEulerAngles = new Vector3(0, angleRotation, 0);
+
+			transform.Rotate(new Vector3(0, horiAxis * sensivity, 0), Space.Self);
+
+			Vector3 newPosition = rb.position + transform.TransformDirection(new Vector3(0, 0, frontAxis * speed * Time.deltaTime));
+			rb.MovePosition(newPosition);
+		}
+		else {
+			float angleRotation = -steerAxis * maxAngle;
+			steer.localEulerAngles = new Vector3(0, angleRotation, 0);
+
+			transform.Rotate(new Vector3(0, -steerAxis * sensivity, 0), Space.Self);
+			if (gasAxis < 0)
+			{
+				Vector3 newPosition = rb.position + transform.TransformDirection(new Vector3(0, 0, -gasAxis * speed * Time.deltaTime));
+				rb.MovePosition(newPosition);
+			}
+
+			if (brakeAxis < 0) {
+				Vector3 newPosition = rb.position + transform.TransformDirection(new Vector3(0, 0, brakeAxis * speed * Time.deltaTime));
+				rb.MovePosition(newPosition);
+			}
 		}
 
-		//transform.Rotate(new Vector3(0, horiAxis * sensivity, 0), Space.Self);
 
-		//Debug.Log(frontAxis);
 
 	}
+
 }
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[System.Serializable]
+public class Controller : MonoBehaviour
+{
+
+	protected enum InputType {
+		Xbox,Steer,Keyboard
+	}
+
+	//protected float verticalAxis;
+	//protected float horizontalAxis;
+	[SerializeField] protected InputType controller;
+	
+	
+}
